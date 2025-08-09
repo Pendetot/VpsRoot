288126e969972199d2176887c028266aed4453c7
LazyDev <142876082+Pendetot@users.noreply.github.com>
2025-08-09 07:06:32 +0700

    Add files via upload

---
+++ vpsroot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+clear;
+echo "==============================================="
+echo "                   LazyDev                     "
+echo "==============================================="
+echo ""
+#echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+wget -qO- -O /etc/ssh/sshd_config https://raw.githubusercontent.com/BangsNgek/JualanSSH/main/sshd_config;
+systemctl restart sshd;
+clear;
+echo -e "Masukkan Password:";
+read -e pwe;
+usermod -p `perl -e "print crypt("$pwe","Q4")"` root;
+clear;
+printf "Mohon Simpan Informasi Akun VPS Ini"
+printf "============================================\n"
+printf "Akun Root (Akun Utama)\n"
+printf "Username   = root\n"
+printf "Password   = \n"
+printf "============================================\n"
+echo "";
+exit;
